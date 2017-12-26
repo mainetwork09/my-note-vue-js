30c3ec389c2cf9af5646dbf4ae47f6cca37d7775
Sirachon Chancharmsri <sirachon@ripplewerkz.com>
2017-12-26 14:47:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        textMessage: '',
+        storeNoteLists: [],
+        showNoteLists: '',
+        count:0
+    },
+    methods: {
+        showNote: function(){
+
+        },
+        addNote: function(){
+            if( this.textMessage ){
+                this.storeNoteLists.push({message:this.textMessage,id:this.count})
+                //this.showNote()
+                this.textMessage = ""
+                this.$refs.inputMessage.focus()
+                this.count++
+            }
+        },
+        deleteNote: function(id){
+            if( this.storeNoteLists.length == 1 ){
+                this.storeNoteLists=[]
+                this.count=0
+            }else{
+                this.storeNoteLists.splice(id,1)
+            }
+            this.$refs.inputMessage.focus()
+        }
+    }
+})
